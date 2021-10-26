c6fc6f9930b444a2f7a8d98da64606c245d1123e
matthijsdz <78727315+matthijsdz@users.noreply.github.com>
2021-10-26 22:27:32 +0200

    Extract-Features added, not finished

--- src/pyevaluationengine/main.py
+++ src/pyevaluationengine/main.py
@@ -1,3 +1,8 @@
+import openml.datasets as dt
+from pymfe.mfe import MFE
+import arff
+import pandas as pd
+import numpy as np
 import requests
 import json
 import os
@@ -24,6 +29,38 @@ def downloadDataset(id):
     response = requests.get("https://test.openml.org/api/v1/json/data/"+id, params={'api_key':api_key})
     url = getUrl(json.loads(response.text))
     open('temp.arff', 'wb').write(requests.get(url).content)
+
+def extractFeatures(id):
+    #extract features
+    dsd = dt.get_dataset(id) # dataset discription
+    default_target = dsd.default_target_attribute
+    X,y,categorical_indicator,attribute_names = dsd.get_data(target=default_target)
+    features = []
+    try:
+        for name, attribute in X.iteritems(): 
+            feature = {}
+            att_index = X.columns.get_loc(name)
+            feature["Index"] = index
+            feature["Name"] = name
+            if index in dsd.get_features_by_type('nominal') :
+                 feature["data_type"] = "nominal"
+            elif index in dsd.get_features_by_type('numeric') :
+                feature["data_type"] = "numeric" 
+            elif index in dsd.get_features_by_type('date') :
+                feature["data_type"] = "date" 
+            elif index in dsd.get_features_by_type('string') :
+                feature["data_type"] = "string" 
+            else:
+                feature["data_type"] = "unknown" 
+    except:
+        print("Error")
+    #extract properties
+    d_X = X.to_numpy()
+    d_y = y.to_numpy()
+    mfe = MFE(groups="all")
+    mfe.fit(d_X, d_y)
+    ft = mfe.extract()
+    #return ft?
 
 def uploadDataset():
     return
