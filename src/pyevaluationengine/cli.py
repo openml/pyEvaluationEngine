15f49be6a6f450b1aec777c6da475ce14769b7e5
monstergems <90658459+monstergems@users.noreply.github.com>
2021-11-23 20:41:41 +0100

    added print function for unprocesd data sets using -p=1

--- src/pyevaluationengine/cli.py
+++ src/pyevaluationengine/cli.py
@@ -1,9 +1,15 @@
 import argparse
 import logging
 import sys
+
+from requests.models import Response
 import evaluationengine
 
+import json
+import requests
 from pyevaluationengine.evaluationengine import EvaluationEngine
+from pyevaluationengine import config
+
 
 from __version__ import __version__ #orgineel (.__version__ import ...) weet niet de reden voor die punt dus heb hem weg gehaald, comment voor het geval dat het dit iets sloopt
 
@@ -40,10 +46,22 @@ def parse_args(args):
         "-times",
         type=int,
         default=0,
-        help="amount of times the evaluation engine is run",
-
+        help="set amount of times the evaluation engine is run",
     )
-
+    parser.add_argument( 
+        "-p",
+        "-print",
+        type=int,
+        default=0,
+        help="Print unprocesed data if its value is higher than 0",
+    )
+    parser.add_argument( 
+        "-a",
+        "-all",
+        type=int,
+        default=0,
+        help="procces all unprocesed data if its value is higher than 0",
+    )
     return parser.parse_args(args)
 
 
@@ -62,13 +80,25 @@ def main(args):
 def run():
     main(sys.argv[1:])
 
+def print_unproccesed_data(url=config.defaults["url"],apikey=config.defaults["apikey"]):
+    response=requests.get(config.testing["url"]+"/data/unprocessed/0/normal", params={"api_key":config.testing["apikey"]})
+    data=json.loads(response.text)
+    datasets=[]
+    print("the following datasets are unprocessed")
+    for i in data["data_unprocessed"]:
+        print(data["data_unprocessed"][i]["name"])
+
 if __name__ == "__main__":
     run()
     amount_of_repeats=(parse_args(sys.argv[1:]).t)
+    print_unproccesed=(parse_args(sys.argv[1:]).p)
+    process=(parse_args(sys.argv[1:]).a)
     if amount_of_repeats > 0: #voert t=x keer eveluationengine.py uit kijk of dit correct is met wat script 2 zou moetten doen
         i=0
         while amount_of_repeats > i:
             i+=1
             _logger.info("executing main function for the "+str(i)+"th time")
             evaluationengine.main()
-            
+    if print_unproccesed>0:
+        print_unproccesed_data()
+
